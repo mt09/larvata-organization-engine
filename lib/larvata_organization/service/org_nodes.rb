97ed3d59dc3101a35e5246ffbd786dd7a90700bb
jamesyu <zetaentranceserious@gmail.com>
2019-05-20 17:43:26 +0800

    feat: 建立部門後加入主管與員工節點

--- lib/larvata_organization/service/org_nodes.rb
+++ lib/larvata_organization/service/org_nodes.rb
@@ -5,6 +5,7 @@ module LarvataOrganization
         def create_company_node(uuid, name, office_address)
           company = LarvataOrganization::Factory::Orgs.create_company(uuid, name, office_address)
           root = LarvataOrganization::Factory::Nodes.create_root(company)
+
           {
             organization: company,
             tree_node: root
@@ -14,9 +15,20 @@ module LarvataOrganization
         def add_org_node(parent_node, typing, name, code)
           org = LarvataOrganization::Factory::Orgs.create_org(typing, name, code)
           node = LarvataOrganization::Factory::Nodes.create_child(parent_node, org)
+          manager_org = LarvataOrganization::Factory::Orgs.create_org('manager', '主管', "#{org.code}_manager")
+          manager_node = LarvataOrganization::Factory::Nodes.create_child(node, manager_org)
+          member_org = LarvataOrganization::Factory::Orgs.create_org('member', '員工', "#{org.code}_member")
+          member_node = LarvataOrganization::Factory::Nodes.create_child(node, member_org)
+
           {
             organization: org,
-            tree_node: node
+            tree_node: {
+              node: node,
+              children: {
+                manager: manager_node,
+                member:  member_node
+              }
+            }
           }
         end
       end
