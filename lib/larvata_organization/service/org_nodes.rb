5e5cb3df1c196c4ff6feba0ee4f3f83e4060e740
jamesyu <zetaentranceserious@gmail.com>
2019-05-20 17:01:52 +0800

    chore: 建立組織與節點 service 方法改為靜態方法

--- lib/larvata_organization/service/org_nodes.rb
+++ lib/larvata_organization/service/org_nodes.rb
@@ -1,22 +1,24 @@
 module LarvataOrganization
   module Service
     class OrgNodes
-      def create_company_node(uuid, name, office_address)
-        company = LarvataOrganization::Factory::Orgs.create_company(uuid, name, office_address)
-        root = LarvataOrganization::Factory::Nodes.create_root(company)
-        {
-          organization: company,
-          tree_node: root
-        }
-      end
+      class << self
+        def create_company_node(uuid, name, office_address)
+          company = LarvataOrganization::Factory::Orgs.create_company(uuid, name, office_address)
+          root = LarvataOrganization::Factory::Nodes.create_root(company)
+          {
+            organization: company,
+            tree_node: root
+          }
+        end
 
-      def add_org_node(parent_node, typing, name, code)
-        org = LarvataOrganization::Factory::Orgs.create_org(typing, name, code)
-        node = LarvataOrganization::Factory::Nodes.create_child(parent_node, org)
-        {
-          organization: org,
-          tree_node: node
-        }
+        def add_org_node(parent_node, typing, name, code)
+          org = LarvataOrganization::Factory::Orgs.create_org(typing, name, code)
+          node = LarvataOrganization::Factory::Nodes.create_child(parent_node, org)
+          {
+            organization: org,
+            tree_node: node
+          }
+        end
       end
     end
   end
