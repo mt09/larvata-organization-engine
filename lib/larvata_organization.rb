aeab92a8ceafa102ec92fbe98f7047d1795e74f7
jamesyu <zetaentranceserious@gmail.com>
2019-05-20 15:33:16 +0800

    feat: 組織節點類別由 host 專案指定

--- lib/larvata_organization.rb
+++ lib/larvata_organization.rb
@@ -2,9 +2,14 @@ module LarvataOrganization
   require "larvata_organization/engine"
 
   mattr_accessor :organization_class
+  mattr_accessor :tree_node_class
 
   def self.organization_class
     @@organization_class.constantize
+  end
+
+  def self.tree_node_class
+    @@tree_node_class.constantize
   end
 
   require 'larvata_organization/trunk/linkable'
--- lib/larvata_organization/factory/nodes.rb
+++ lib/larvata_organization/factory/nodes.rb
@@ -2,9 +2,8 @@ module LarvataOrganization
   module Factory
     class Nodes
       class << self
-        def create_root(nodeable)
-          org = nodeable[:nodeable]
-          TreeNode.company.enabled.create(nodeable: org, code: org.uuid, name: org.abbrev_name)
+        def create_root(org)
+          LarvataOrganization.tree_node_class.company.enabled.create(nodeable: org, code: org.uuid, name: org.abbrev_name)
         end
       end
     end
