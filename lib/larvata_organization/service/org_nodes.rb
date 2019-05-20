509e63744b30fc64eb4c42223fd96d59d024ce8e
jamesyu <zetaentranceserious@gmail.com>
2019-05-20 16:49:05 +0800

    feat: 設計建立公司與公司組織根節點 service

--- lib/larvata_organization/service/org_nodes.rb
+++ lib/larvata_organization/service/org_nodes.rb
@@ -1,6 +1,17 @@
 module LarvataOrganization
   module Service
     class OrgNodes
+      def create_company_node(uuid, name, office_address)
+        company = LarvataOrganization::Factory::Orgs.create_company(uuid, name, office_address)
+        root = LarvataOrganization::Factory::Nodes.create_root(company)
+        {
+          organization: company,
+          tree_node: root
+        }
+      end
+
+      def add_org_node()
+      end
     end
   end
 end
