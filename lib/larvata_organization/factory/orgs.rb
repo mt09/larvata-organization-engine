698001756147811a858a0da727b4cea4cbf914c4
jamesyu <zetaentranceserious@gmail.com>
2019-05-20 12:06:49 +0800

    feat: 初步建立公司資料方法

--- lib/larvata_organization/factory/orgs.rb
+++ lib/larvata_organization/factory/orgs.rb
@@ -1,6 +1,16 @@
 module LarvataOrganization
   module Factory
     class Orgs
+      class << self
+        def create_company(uuid, name, office_address)
+          Organization.company.enabled.create(
+            uuid: uuid,
+            abbrev_name: name,
+            full_name: name,
+            code: "company_#{uuid}"
+          )
+        end
+      end
     end
   end
 end
