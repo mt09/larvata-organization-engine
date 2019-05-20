946d66eaf1a3e19806919f329d973af86f12cd7e
jamesyu <zetaentranceserious@gmail.com>
2019-05-20 14:50:17 +0800

    feat: 初步建立公司組織樹根節點

--- lib/larvata_organization/factory/nodes.rb
+++ lib/larvata_organization/factory/nodes.rb
@@ -1,6 +1,12 @@
 module LarvataOrganization
   module Factory
     class Nodes
+      class << self
+        def create_root(nodeable)
+          org = nodeable[:nodeable]
+          TreeNode.company.enabled.create(nodeable: org, code: org.uuid, name: org.abbrev_name)
+        end
+      end
     end
   end
 end
